d7d277ac6070d30046b9a19fc7ca625893698841
oumbarae hachmahocine <hachmaoui@hotmail.fr>
2022-09-26 12:06:29 +0200

    Phase 7:ORM  (génération automatique d'enregistrements).

---
+++ src/DataFixtures/VisiteFixture.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Visite;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class VisiteFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker=Factory::create('fr_FR');
+        for($k=0;$k<100;$k++){
+            $visite=new visite ();
+            $visite->setville($faker->city)
+                   ->setpays($faker->country)
+                   ->setDatecreation($faker->dateTimeBetween('-10 years','now'))
+                    ->setTempmin($faker->numberBetween(-20,10))
+                    ->setTempmax($faker->numberBetween(10,40))
+                    ->setNote($faker->numberBetween(0,20))
+                    ->setAvis($faker->sentences(4,TRUE));
+            $manager->persist($visite);
+        }
+
+        $manager->flush();
+    }
+}
