943dbb383340a0ccfe6edc1609a209519bec9dca
oumbarae hachmahocine <hachmaoui@hotmail.fr>
2022-10-14 12:17:17 +0200

    Phase 9:ORM  (tri des enregistrements).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -5,8 +5,10 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\VisiteRepository;
+
 /**
  * Description of VoyagesController
  *
@@ -35,10 +37,24 @@ public function __construct(VisiteRepository $repository) {
      * @return Response
      */
     public function index():Response {
-        $visites = $this->repository->findall();
+        $visites = $this->repository->findAllOrderBy('datecreation','DESC');
     return $this->render("pages/voyages.html.twig",[
         'visites' => $visites
     ]);
+   }
+   /**
+    * @Route("/voyages/tri/{champ}/{ordre}", name="voyages.sort")
+    * @param type $champ
+    * @param type $ordre
+    * @return Response
+    */
+   
+   public function sort($champ,$ordre):Response {
+       $visites=$this->repository->findAllOrderBy($champ,$ordre);
+       return $this->render("pages/voyages.html.twig",[
+           'visites'=>$visites
+       ]);
+       
    }
 }
 
