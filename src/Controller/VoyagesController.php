45f4fa3ec6c94162897919741077cb3774ee05f3
oumbarae hachmahocine <hachmaoui@hotmail.fr>
2022-10-18 10:03:52 +0200

    Phase 11:ORM  (nouvelle page ppur affichage de taille).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -69,5 +69,17 @@ public function __construct(VisiteRepository $repository) {
            'visites'=> $visites
        ]);
    }
+   
+   /**
+    * @Route("/voyages/voyage/{id}",name="voyages.showone")
+    * @param type $id
+    * @return Response
+    */
+   public function showOne($id):Response{
+       $visite = $this->repository->find($id);
+       return $this->render ("pages/voyage.html.twig",[
+           'visite'=>$visite
+       ]);
+   }
 }
 
